3d853c5ad9bb327f75e7db28a3942ff6a62f4a6b
Lucas Simionato <simionato.lucas99@gmail.com>
2022-12-16 10:15:47 -0300

    added ui and styles

---
+++ main.js
@@ -0,0 +1,29 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(op, a, b) {
+    switch (op) {
+        case "+":
+            return add(a, b);
+        case "-":
+            return subtract(a, b);
+        case "*":
+            return multiply(a, b);
+        case "/":
+            return divide(a, b); 
+    }
+}
+
